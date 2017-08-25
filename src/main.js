031ef52ad791e1e402d7ec7ca11c6ac1759885f7
Blake Tarter <blake@codekoalas.com>
2017-08-25 18:53:10 -0500

    wip

--- src/main.js
+++ src/main.js
@@ -18,8 +18,8 @@ const CELLS = {
   },
   WATER: {
     value: 2,
-    color: '#4f8fff'
-  }
+    color: '#4f8fff',
+  },
 };
 
 function getRowColumn(index, columns, rows) {
@@ -30,7 +30,10 @@ function getRowColumn(index, columns, rows) {
 }
 
 function getXY(column, row, cellHeight, cellWidth) {
-  return [(column * cellWidth) + (cellWidth / 2), (row * cellHeight) + (cellHeight / 2)];
+  return [
+    column * cellWidth + cellWidth / 2,
+    row * cellHeight + cellHeight / 2,
+  ];
 }
 
 function initCanvas(canvasEle, height, width) {
@@ -76,16 +79,29 @@ function initCells(rows, columns, initialCells = []) {
 }
 
 function cellsWithMeta(cells, columns, rows) {
-  return addMetaToMap(cells, (cellType, index, rows, columns) => {
-    return {
-      type: CELLS[CELL_TYPES[cellType]],
-      index: index,
-      coords: getRowColumn(index, columns, rows),
-    };
-  }, rows, columns);
+  return addMetaToMap(
+    cells,
+    (cellType, index, rows, columns) => {
+      return {
+        type: CELLS[CELL_TYPES[cellType]],
+        index: index,
+        coords: getRowColumn(index, columns, rows),
+      };
+    },
+    rows,
+    columns,
+  );
 }
 
-function drawCell(index, columns, rows, cellHeight, cellWidth, cell, outline = false) {
+function drawCell(
+  index,
+  columns,
+  rows,
+  cellHeight,
+  cellWidth,
+  cell,
+  outline = false,
+) {
   const [column, row] = getRowColumn(index, columns, rows);
 
   ctx.fillStyle = cell.type.color;
@@ -95,7 +111,14 @@ function drawCell(index, columns, rows, cellHeight, cellWidth, cell, outline = f
 
   if (outline) {
     ctx.strokeRect(column * cellWidth, row * cellHeight, cellHeight, cellWidth);
-    drawText(index, columns, rows, cellHeight, cellWidth, `${index}:[${column}, ${row}]`);  
+    drawText(
+      index,
+      columns,
+      rows,
+      cellHeight,
+      cellWidth,
+      `${index}:[${column}, ${row}]`,
+    );
   }
 }
 
@@ -109,8 +132,8 @@ function drawText(index, columns, rows, cellHeight, cellWidth, text) {
 
   const textWidth = ctx.measureText(text);
 
-  ctx.strokeText(text, x - (textWidth.width / 2), y + (5 / 2), cellWidth);
-  ctx.fillText(text, x - (textWidth.width / 2), y + (5 / 2), cellWidth);
+  ctx.strokeText(text, x - textWidth.width / 2, y + 5 / 2, cellWidth);
+  ctx.fillText(text, x - textWidth.width / 2, y + 5 / 2, cellWidth);
 }
 
 function renderCells(cells, rows, columns, cellHeight, cellWidth) {
@@ -120,9 +143,14 @@ function renderCells(cells, rows, columns, cellHeight, cellWidth) {
 }
 
 // cells, rows, columns, fillPercent, cellTypeToReplace, cellTypeToFill
-function fillCells(cells, fillPercent = 0.5, cellTypeToReplace = 0, cellTypeToFill = 1) {
+function fillCells(
+  cells,
+  fillPercent = 0.5,
+  cellTypeToReplace = 0,
+  cellTypeToFill = 1,
+) {
   const newCells = [];
-  
+
   for (let i = 0, ii = cells.length; i < ii; i++) {
     if (cells[i] !== cellTypeToReplace) {
       newCells.push(cells[i]);
@@ -139,11 +167,19 @@ function fillCells(cells, fillPercent = 0.5, cellTypeToReplace = 0, cellTypeToFi
   return newCells;
 }
 
-function drawLine(fromIndex, toIndex, columns, rows, cellHeight, cellWidth, color) {
+function drawLine(
+  fromIndex,
+  toIndex,
+  columns,
+  rows,
+  cellHeight,
+  cellWidth,
+  color,
+) {
   const [fromColumn, fromRow] = getRowColumn(fromIndex, columns, rows);
   const [toColumn, toRow] = getRowColumn(toIndex, columns, rows);
   const oldLineWidth = ctx.lineWidth;
-  
+
   ctx.strokeStyle = 'black';
   ctx.lineWidth = 5;
 
@@ -176,21 +212,24 @@ function getNeighbors(index, cells, rows, columns) {
   const bottomDelta = columns;
   const topDelta = 0 - bottomDelta;
   const rightDelta = 1;
-  const leftDelta = - rightDelta;
+  const leftDelta = -rightDelta;
 
   const topCell = cells[index + topDelta];
   const bottomCell = cells[index + bottomDelta];
   let leftCell;
   let rightCell;
 
-  if (column !== 0) { // probably has a left cell
+  if (column !== 0) {
+    // probably has a left cell
     leftCell = cells[index + leftDelta];
   }
 
-  if (leftCell && topCell) { // if there is a leftCell and a topCell there is probably a topLeftCell
+  if (leftCell && topCell) {
+    // if there is a leftCell and a topCell there is probably a topLeftCell
     const topLeftCell = cells[index + (topDelta + leftDelta)];
 
-    if (topLeftCell) { // make sure there really is a topLeftCell before pushing it
+    if (topLeftCell) {
+      // make sure there really is a topLeftCell before pushing it
       neighbors.push(topLeftCell);
     } else {
       neighbors.push(undefined);
@@ -205,14 +244,17 @@ function getNeighbors(index, cells, rows, columns) {
     neighbors.push(undefined);
   }
 
-  if (column !== columns - 1) { // probably has a right cell
+  if (column !== columns - 1) {
+    // probably has a right cell
     rightCell = cells[index + rightDelta];
   }
 
-  if (rightCell && topCell) { // if there is a rightCell and a topCell there is probably a topRightCell
+  if (rightCell && topCell) {
+    // if there is a rightCell and a topCell there is probably a topRightCell
     const topRightCell = cells[index + (topDelta + rightDelta)];
 
-    if (topRightCell) { // make sure there really is a topRightCell before pushing it
+    if (topRightCell) {
+      // make sure there really is a topRightCell before pushing it
       neighbors.push(topRightCell);
     } else {
       neighbors.push(undefined);
@@ -227,10 +269,12 @@ function getNeighbors(index, cells, rows, columns) {
     neighbors.push(undefined);
   }
 
-  if (rightCell && bottomCell) { // if there is a rightCell and a bottomCell there is probably a bottomRightCell
+  if (rightCell && bottomCell) {
+    // if there is a rightCell and a bottomCell there is probably a bottomRightCell
     const bottomRightCell = cells[index + (bottomDelta + rightDelta)];
 
-    if (bottomRightCell) { // make sure there really is a bottomRightCell before pushing it
+    if (bottomRightCell) {
+      // make sure there really is a bottomRightCell before pushing it
       neighbors.push(bottomRightCell);
     } else {
       neighbors.push(undefined);
@@ -245,7 +289,8 @@ function getNeighbors(index, cells, rows, columns) {
     neighbors.push(undefined);
   }
 
-  if (leftCell && bottomCell) { // if there is a leftCell and a bottomCell there is probably a bottomLeftCell
+  if (leftCell && bottomCell) {
+    // if there is a leftCell and a bottomCell there is probably a bottomLeftCell
     const bottomLeftCell = cells[index + (bottomDelta + leftDelta)];
 
     if (bottomLeftCell) {
@@ -266,17 +311,30 @@ function getNeighbors(index, cells, rows, columns) {
   return neighbors;
 }
 
-export default function genenerate(columns = 10, rows = 10, cellHeight = 50, cellWidth = 50, fillPercent = 0.5, initialCells = [], cellTypeToReplace = 0, cellTypeToFill = 1) {
+export default function genenerate(
+  columns = 10,
+  rows = 10,
+  cellHeight = 50,
+  cellWidth = 50,
+  fillPercent = 0.5,
+  initialCells = [],
+  cellTypeToReplace = 0,
+  cellTypeToFill = 1,
+) {
   const MAP = initMap(rows, columns, cellHeight, cellWidth);
-  ctx = initCanvas(canvas, MAP.height, MAP.width)
+  ctx = initCanvas(canvas, MAP.height, MAP.width);
 
   let cells = initCells(rows, columns, initialCells);
 
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, canvas.height, canvas.width);
 
-  cells = cellsWithMeta(fillCells(cells, fillPercent, cellTypeToReplace, cellTypeToFill), rows, columns);
-  
+  cells = cellsWithMeta(
+    fillCells(cells, fillPercent, cellTypeToReplace, cellTypeToFill),
+    rows,
+    columns,
+  );
+
   renderCells(cells, rows, columns, MAP.cellHeight, MAP.cellWidth);
 
   console.log(MAP);
@@ -298,27 +356,47 @@ export default function genenerate(columns = 10, rows = 10, cellHeight = 50, cel
 
       return newCells;
     },
-    step: (stateDecider) => {
-      cells = cells.map((cell) => {
-        const newState = stateDecider(cell, getNeighbors(cell.index, cells, rows, columns));
+    step: stateDecider => {
+      cells = cells.map(cell => {
+        const newState = stateDecider(
+          cell,
+          getNeighbors(cell.index, cells, rows, columns),
+        );
 
         return Object.assign({}, cell, { type: CELLS[CELL_TYPES[newState]] });
       });
 
       renderCells(cells, rows, columns, MAP.cellHeight, MAP.cellWidth);
     },
-    debug: (i) => {
+    debug: i => {
       const neighbors = getNeighbors(i, cells, rows, columns);
 
       console.log(neighbors);
 
       neighbors.forEach((cell, index) => {
-        const orangeCell = Object.assign({}, cell, { type: { color: 'orange' } });
-        drawCell(cell.index, columns, rows, MAP.cellHeight, MAP.cellWidth, orangeCell);
+        const orangeCell = Object.assign({}, cell, {
+          type: { color: 'orange' },
+        });
+        drawCell(
+          cell.index,
+          columns,
+          rows,
+          MAP.cellHeight,
+          MAP.cellWidth,
+          orangeCell,
+        );
       });
 
       neighbors.reduce((prev, next) => {
-        drawLine(prev.index, next.index, columns, rows, MAP.cellHeight, MAP.cellWidth, 'white');
+        drawLine(
+          prev.index,
+          next.index,
+          columns,
+          rows,
+          MAP.cellHeight,
+          MAP.cellWidth,
+          'white',
+        );
         return next;
       });
 
